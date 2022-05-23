8884b1fc53f03e7d1b200bf2560e3d3655664070
sanjeev.thapa <stedwardson@gmail.com>
2022-05-23 14:26:45 +0545

    Customize Error

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -2,11 +2,21 @@
 
 namespace App\Exceptions;
 
-use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
+use App\Traits\SystemResponse;
+use Exception;
+use Illuminate\Auth\AuthenticationException;
 use Throwable;
+use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
+use Illuminate\Http\Exceptions\HttpResponseException;
+use Illuminate\Validation\ValidationException;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpKernel\Exception\MethodNotAllowedHttpException;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 
 class Handler extends ExceptionHandler
 {
+    use SystemResponse;
+
     /**
      * A list of exception types with their corresponding custom log levels.
      *
@@ -46,5 +56,43 @@ class Handler extends ExceptionHandler
         $this->reportable(function (Throwable $e) {
             //
         });
+
+        $this->renderable(function (Exception $e, $request) {
+            if($e instanceof ValidationException){
+                return $this->error($e->errors(), Response::HTTP_UNPROCESSABLE_ENTITY);
+            }
+
+            return $this->error($this->getMessage($e), $this->getStatusCode($e));
+        });
+    }
+
+    private function getMessage($exception)
+    {
+        if(!empty($exception->getMessage())) {
+            return $exception->getMessage();
+        }
+
+        if(!empty($exception->getStatusCode()) && array_key_exists($exception->getStatusCode(), Response::$statusTexts)) {
+            return Response::$statusTexts[$exception->getStatusCode()];
+        }
+
+        return "Internal Server Error";
+    }
+
+    private function getStatusCode($exception)
+    {
+        if(method_exists($exception, 'getStatusCode') && !empty($exception->getStatusCode())) {
+            return $exception->getStatusCode();
+        }
+
+        if(method_exists($exception, 'getCode') && !empty($exception->getCode())) {
+            return $exception->getCode();
+        }
+
+        if($exception instanceof AuthenticationException) {
+            return Response::HTTP_UNAUTHORIZED;
+        }
+
+        return 500;
     }
 }
